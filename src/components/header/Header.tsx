359e769ef5f2f4589bce7b7672c11c2579c76af2
Arshad0899 <arshadbasha1999@outlook.in>
2024-02-10 22:26:11 +0530

    Added nav items in header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -5,36 +5,33 @@ import companyLogo from "../../assets/header-assets/company-logo.svg";
 const Header: React.FC = () => {
 
     const textInfo: string = "- Pearl Polyurethane, Covestro sign climate-neutral MDI supply deal AkzoNobel to expand water-based texture paints capacity in China - HRS showcases restart of the chemical industry in India";
+    const navElements = [
+        'Posts', 'Sectors', 'Insights', 'Spotlight', 'Product', 'Events', 'Equipment', 'Sustainability', 'Digitalization'];
 
     return (
         <>
             <header className="header">
                 <div className='logo-container'>
-                    <img className="logo" src = { companyLogo } alt="Chemical Today"/>
+                    <img className="logo" src={companyLogo} alt="Chemical Today" />
                 </div>
                 <div className='header-info'>
                     <p className='text-info'>
-                        { textInfo }
+                        {textInfo}
                     </p>
                 </div>
-                <nav>
-                    <ul className="navigation">
-                        <li className='item'><a href="/posts">Posts</a></li>
-                        <li className='item'><a href="/sectors">Sectors</a></li>
-                        <li><a href="/insights">Insights</a></li>
-                        <li><a href="/spotlight">Spotlight</a></li>
-                        <li><a href="/product">Product</a></li>
-                        <li><a href="/events">Events</a></li>
-                        <li><a href="/equipment">Equipment</a></li>
-                        <li><a href="/sustainability">Sustainability</a></li>
-                        <li><a href="/digitalization">Digitalization</a></li>
-                    </ul>
-                </nav>
-                <div className="header-actions">
+
+                <div className='navigation'>
+                    {navElements.map((text) => (
+                        <a className='item'>{text}</a>
+                
+                    ))}
+                </div>
+
+                {/* <div className="header-actions">
                     <button className="btn-download">Download the App</button>
                     <button className="btn-subscribe">Magazine Subscribe</button>
                     <button className="btn-tv">Chemical Today TV</button>
-                </div>
+                </div> */}
             </header>
         </>
     );
