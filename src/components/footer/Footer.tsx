63a53b1f19d78a0292e60260d40d10212eb5f61c
Arshad0899 <arshadbasha1999@outlook.in>
2024-02-11 13:53:14 +0530

    footer changes

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -5,8 +5,21 @@ const Footer: React.FC = () => {
 
     return (
         <>
-            <div className="footer">
-                <p>NewsPaper</p>
+
+            <div className="footer-main">
+
+                <div className="footer">
+                    <div className="subscribe-text">
+                        <p>Subscribe to our Newsletter</p>
+                        <p>Stay updated with the latest chemical industry trends and innovations.</p>
+                    </div>
+
+                    <div className="subscribe-input">
+                        <input className="input-feild" placeholder="Email Address:"/>
+                        <button>Subscribe</button>
+                    </div>
+
+                </div>
             </div>
         </>
     )
